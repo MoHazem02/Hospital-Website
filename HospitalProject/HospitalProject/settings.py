f914438bcebf7a9a4eb9f3c01da53b6922d90991
tahaaa22 <ahmed.taha.11oman@gmail.com>
2024-01-04 09:43:19 +0200

    fixed last bugs

--- HospitalProject/HospitalProject/settings.py
+++ HospitalProject/HospitalProject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1Theatree*',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
